8c6cc9f730658183b85a996d64e9d82098e95c4f
cuisj <cuisj@126.com>
2018-03-27 11:30:34 +0800

    实现桥接功能

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
-	"log"
-	"os"
-	"path"
-	"io"
-	"io/ioutil"
-	"net"
 	"fmt"
 	"golang.org/x/crypto/ssh"
-	"golang.org/x/crypto/ssh/terminal"
+	"io"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"path"
+	//"golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
@@ -27,19 +27,23 @@ func main() {
 		log.Fatalln("Parse private key sshfm failed")
 	}
 
-	serverConfig := &ssh.ServerConfig {
-				PasswordCallback: func(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
-						permissions := &ssh.Permissions{CriticalOptions: map[string]string{"force-command": "list"}}
-						return permissions, nil
-					}}
+	serverConfig := &ssh.ServerConfig{
+		PasswordCallback: func(conn ssh.ConnMetadata, password []byte) (*ssh.Permissions, error) {
+			extensions := map[string]string{"username": "hago",
+				"password":  "123",
+				"sshserver": "10.21.16.202:22"}
+			permissions := &ssh.Permissions{Extensions: extensions}
+
+			return permissions, nil
+		}}
 	serverConfig.AddHostKey(hostKey)
 
-	listener, err := net.Listen("tcp", ":6080")
+	listener, err := net.Listen("tcp", ":6001")
 	if err != nil {
-		log.Fatalln("Listen on 6080 failed")
+		log.Fatalln("Listen on 6001 failed")
 	}
 
-	log.Println("Listening on 6080...")
+	log.Println("Listening on 6001...")
 	for {
 		conn, err := listener.Accept()
 		if err != nil {
@@ -47,67 +51,110 @@ func main() {
 			continue
 		}
 
-		serverConn, newChannel, globalRequest, err := ssh.NewServerConn(conn, serverConfig)
+		serverConn, newChanChan, requestChan, err := ssh.NewServerConn(conn, serverConfig)
 		if err != nil {
 			log.Printf("Handshake failed, %s\n", err)
 			continue
 		}
 
-		go handleConnection(serverConn, newChannel, globalRequest)
+		p := &Proxy{serverConn: serverConn, newChanChan: newChanChan, requestChan: requestChan}
+		go p.handle()
 	}
 }
 
-func handleConnection(serverConn *ssh.ServerConn, newChannel <-chan ssh.NewChannel, globalRequest <-chan *ssh.Request) {
-	defer serverConn.Wait()
+type Proxy struct {
+	serverConn  *ssh.ServerConn
+	newChanChan <-chan ssh.NewChannel
+	requestChan <-chan *ssh.Request
 
-	log.Printf("New SSH connection from %s(%s)\n", serverConn.RemoteAddr(), serverConn.User())
+	channel ssh.Channel
+	request <-chan *ssh.Request
 
-	go ssh.DiscardRequests(globalRequest)
+	client  *ssh.Client
+	session *ssh.Session
+}
 
-	for newChannel := range newChannel {
-		if chanType := newChannel.ChannelType(); chanType != "session" {
-			newChannel.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", chanType))
+func (p *Proxy) handle() {
+	defer p.serverConn.Wait()
+
+	log.Printf("connection: [%s] [%s]\n", p.serverConn.RemoteAddr(), p.serverConn.User())
+
+	// 忽略全局请求
+	go ssh.DiscardRequests(p.requestChan)
+
+	// 连接远程服务端
+	clientConfig := &ssh.ClientConfig{
+		User: p.serverConn.Permissions.Extensions["username"],
+		Auth: []ssh.AuthMethod{
+			ssh.Password(p.serverConn.Permissions.Extensions["password"]),
+		},
+
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	client, err := ssh.Dial("tcp", p.serverConn.Permissions.Extensions["sshserver"], clientConfig)
+	if err != nil {
+		return
+	}
+	defer client.Close()
+
+	// 处理通道请求, 我们只处理session请求
+	for newChan := range p.newChanChan {
+		if chanType := newChan.ChannelType(); chanType != "session" {
+			newChan.Reject(ssh.UnknownChannelType, fmt.Sprintf("unknown channel type: %s", chanType))
 			continue
 		}
 
-		channel, request, err := newChannel.Accept()
+		ch, req, err := newChan.Accept()
 		if err != nil {
 			log.Printf("Accept channel failed, %s\n", err)
 			continue
 		}
 
-		handleChannel(serverConn, channel, request)
+		session, err := client.NewSession()
+		if err != nil {
+			log.Printf("Failed to create session: ", err)
+			return
+		}
+
+		p.channel = ch
+		p.request = req
+
+		p.client = client
+		p.session = session
+
+		break
 	}
+
+	p.handleChannel()
 }
 
-func handleChannel(serverConn *ssh.ServerConn, channel ssh.Channel, requests <-chan *ssh.Request) {
-	originalRequests := make(chan *ssh.Request, 16)
+func (p *Proxy) handleChannel() {
+	defer p.session.Close()
+	defer p.channel.Close()
 
 	go func() {
-		for request := range requests {
-			originalRequests <- request
+		for request := range p.request {
+			result, err := p.session.SendRequest(request.Type, request.WantReply, request.Payload)
+			if err != nil {
+				log.Printf("Send request failed, %v", err)
+			}
 
 			if request.WantReply {
-				request.Reply(true, nil)
+				request.Reply(result, nil)
 			}
 		}
-
-		close(originalRequests)
 	}()
 
-	go func() {
-		for request := range originalRequests {
-			log.Printf("%s\n", request.Type)
-		}
-	}()
+	stdout, _ := p.session.StdoutPipe()
+	stderr, _ := p.session.StderrPipe()
+	stdin, _ := p.session.StdinPipe()
 
+	go io.Copy(stdin, p.channel)
+	go io.Copy(p.channel, stdout)
+	io.Copy(p.channel, stderr)
 
-	term := terminal.NewTerminal(channel, "fm> ")
-	defer channel.Close()
-
-	fmt.Fprintln(term, "Welcome to Fortress Machine, Your resource list below:")
-	fmt.Fprintln(term, "[1] 10.21.16.202")
-
+	/*term := terminal.NewTerminal(p.channel, "fm> ")
 	for {
 		line, err := term.ReadLine()
 		if err != nil {
@@ -123,5 +170,5 @@ func handleChannel(serverConn *ssh.ServerConn, channel ssh.Channel, requests <-c
 		}
 
 		fmt.Fprintln(term, "Just to controll you!")
-	}
+	}*/
 }
